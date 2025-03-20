7c38a017dcf26cf27674d8c3237ac89f524eb5db
prem-thatikonda29 <2023.tprem@isu.ac.in>
2025-03-20 21:35:13 +0530

    resized footer

--- src/organisms/Footer.jsx
+++ src/organisms/Footer.jsx
@@ -6,23 +6,19 @@ function Footer() {
   const icons = [Instagram, Linkedin, Github, Twitter];
 
   return (
-    <div className="w-full h-auto min-h-[15vh] py-8 md:py-0 bg-background-light dark:bg-background border-t border-background dark:border-background-light shadow-lg shadow-background dark:shadow-background-light flex items-center justify-center gap-4 sm:gap-6">
+    <div className="w-full min-h-[15vh] py-6 sm:py-8 bg-background-light dark:bg-background border-t border-background dark:border-background-light shadow-lg flex flex-wrap items-center justify-center gap-6 sm:gap-8">
       {icons.map((Icon, index) => (
         <Tilt
           key={index}
-          tiltMaxAngleX={35}
-          tiltMaxAngleY={35}
+          tiltMaxAngleX={25}
+          tiltMaxAngleY={25}
           perspective={1000}
-          scale={1.1}
-          transitionSpeed={1000}
+          scale={1.05}
+          transitionSpeed={800}
           glareEnable={false}
-          className="group relative w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] md:w-[70px] md:h-[70px] flex justify-center items-center border-2 border-background dark:border-background-light transition-transform duration-500 transform-style-3d"
+          className="group w-[45px] h-[45px] sm:w-[55px] sm:h-[55px] md:w-[65px] md:h-[65px] flex justify-center items-center border-2 border-background dark:border-background-light transition-all duration-500"
         >
-          {/* Icon */}
-          <Icon
-            size={28}
-            className="text-background dark:text-background-light cursor-pointer relative z-10 sm:size-32 md:size-32"
-          />
+          <Icon className="text-background dark:text-background-light cursor-pointer size-5 sm:size-6 md:size-7" />
         </Tilt>
       ))}
     </div>
